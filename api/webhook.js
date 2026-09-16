b3a855dcc750f0d36965802d729d507739284811
moreand204-dot <moreand204@gmail.com>
2026-09-16 22:06:50 +0300

    Create webhook.js

---
+++ api/webhook.js
@@ -0,0 +1,20 @@
+export default function handler(req, res) {
+  const VERIFY_TOKEN = 'oracle123';
+
+  if (req.method === 'GET') {
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+    
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      return res.status(200).send(challenge);
+    } else {
+      return res.status(403).send('Forbidden');
+    }
+  }
+
+  if (req.method === 'POST') {
+    console.log(JSON.stringify(req.body, null, 2));
+    return res.status(200).send('EVENT_RECEIVED');
+  }
+}
